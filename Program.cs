135140134912da4f3e1ecb98359938e22f148327
Александр Небайкин <iamarokk@gmail.com>
2020-05-15 22:44:07 +0300

    Inti

--- Program.cs
+++ Program.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace TestMiddleDB
 {
     class Program
     {
+        public static List<User> models;
+        public User FindUser { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            program.Start();
+        }
+
+        public void Start()
+        {
+
+            models = new List<User>();
+            ParallelLoopResult loopResult = ProcessFile("user.json");
+            Console.WriteLine(loopResult.IsCompleted);
+            if (loopResult.IsCompleted == true)
+            {
+                foreach (User item in models)
+                {
+                    using (ApplicationContext db = new ApplicationContext())
+                    {
+                        Console.WriteLine($"Name: {item.FIO}  Age: {item.Date}");
+                        FindUser = db.Users.Find(item.Identity);
+                        if (FindUser == null)
+                        {
+                            //db.Models.Find(item)
+                            db.Users.Add(item);
+                            db.SaveChanges();
+                        }
+
+                    }
+                }
+
+            }
+        }
+
+        public static void ReadString(string json)
+        {
+            models = JsonConvert.DeserializeObject<List<User>>(json);
+            //models.Add(model);
+            //Console.WriteLine($"Name: {model.FIO}  Age: {model.Date}");
+        }
+
+        public static ParallelLoopResult ProcessFile(string path)
+        {
+            ParallelLoopResult loopResult = Parallel.ForEach(File.ReadLines(path), line =>
+            {
+                ReadString(line);
+            });
+            return loopResult;
         }
     }
 }
